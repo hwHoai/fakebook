9253528f49506222a64c1a49ace74e294f78202e
Hữu Hoài <147300113+hwHoai@users.noreply.github.com>
2025-06-24 16:58:38 +0700

    Update firebaseStorage.js

--- fe/src/config/firebaseStorage.js
+++ fe/src/config/firebaseStorage.js
@@ -7,13 +7,13 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAbAF6dRwlYW4kn5EHFHy21lu_7Q6k3h0U',
-  authDomain: 'fakebook-5b337.firebaseapp.com',
-  projectId: 'fakebook-5b337',
-  storageBucket: 'fakebook-5b337.firebasestorage.app',
-  messagingSenderId: '664488014066',
-  appId: '1:664488014066:web:aa20d3f0f52bd35d3b206b',
-  measurementId: 'G-WK65HS5C39',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 // Initialize Firebase
